06177f8e626a4ea80f64bd0f06ded76918cc9c35
lorenzodrago <43404051+lorenzodrago@users.noreply.github.com>
2020-06-19 00:56:01 +0200

    added carousel images, animations, fixed mobile carousel

--- script.js
+++ script.js
@@ -4,4 +4,39 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
-$(".navbar-default").addClass('scrolled');
+
+var topStarted = false;
+$('.choice.top').mouseenter(function(){   
+    if(topStarted==false){
+        topStarted=true;
+        $('.choice.top').addClass('pressed');
+        $('.choice.bottom').addClass('depressed');
+        $('.choice.top .choice-img').addClass('animated');
+        setTimeout(function(){
+            
+            $('.choice.top .choice-img').removeClass('animated');
+            topStarted=false;
+        },700);  
+    }
+});
+$('.choice.top').mouseleave(function(){
+    $('.choice.top').removeClass('pressed');
+    $('.choice.bottom').removeClass('depressed');
+});
+var bottomStarted = false;
+$('.choice.bottom').mouseenter(function(){   
+    if(bottomStarted==false){
+        bottomStarted=true;
+        $('.choice.bottom').addClass('pressed');
+        $('.choice.top').addClass('depressed');
+        $('.choice.bottom .choice-img').addClass('animated');
+        setTimeout(function(){
+            $('.choice.bottom .choice-img').removeClass('animated');
+            bottomStarted=false;
+        },700);  
+    }
+});
+$('.choice.bottom').mouseleave(function(){
+    $('.choice.bottom').removeClass('pressed');
+    $('.choice.top').removeClass('depressed');
+});

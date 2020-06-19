bd0bf978db6547dfeabc5d153ce9d7d8f514ef5f
lorenzodrago <43404051+lorenzodrago@users.noreply.github.com>
2020-06-19 10:39:28 +0200

    added doodles, fixed animation

--- script.js
+++ script.js
@@ -5,36 +5,22 @@ $(function () {
   });
 });
 
-var topStarted = false;
+
 $('.choice.top').mouseenter(function(){   
-    if(topStarted==false){
-        topStarted=true;
-        $('.choice.top').addClass('pressed');
-        $('.choice.bottom').addClass('depressed');
-        $('.choice.top .choice-img').addClass('animated');
-        setTimeout(function(){
-            
-            $('.choice.top .choice-img').removeClass('animated');
-            topStarted=false;
-        },700);  
-    }
+    $('.choice.top').addClass('pressed');
+    $('.choice.bottom').addClass('depressed');
+    $('.choice.top .choice-img').removeClass('animated');
+    setTimeout(function() {$('.choice.top .choice-img').addClass('animated')}, 10);
 });
 $('.choice.top').mouseleave(function(){
     $('.choice.top').removeClass('pressed');
     $('.choice.bottom').removeClass('depressed');
 });
-var bottomStarted = false;
 $('.choice.bottom').mouseenter(function(){   
-    if(bottomStarted==false){
-        bottomStarted=true;
-        $('.choice.bottom').addClass('pressed');
-        $('.choice.top').addClass('depressed');
-        $('.choice.bottom .choice-img').addClass('animated');
-        setTimeout(function(){
-            $('.choice.bottom .choice-img').removeClass('animated');
-            bottomStarted=false;
-        },700);  
-    }
+    $('.choice.bottom').addClass('pressed');
+    $('.choice.top').addClass('depressed');
+    $('.choice.bottom .choice-img').removeClass('animated');
+    setTimeout(function() {$('.choice.bottom .choice-img').addClass('animated')}, 10);
 });
 $('.choice.bottom').mouseleave(function(){
     $('.choice.bottom').removeClass('pressed');
